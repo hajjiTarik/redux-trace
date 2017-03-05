4ed4d72dea5734be0e9f21f1b0eb0ab9e5d2ba4f
hajji tarek <tahajji@sqli.com>
2017-03-05 01:43:30 +0000

    lint + webpack config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": ["error", 2],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+
+        ]
+    }
+};
--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,97 @@
-"use strict";
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["redux-trace"] = factory();
+	else
+		root["redux-trace"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var reTrace = function reTrace() {
+	  var _this = this;
+
+	  _classCallCheck(this, reTrace);
+
+	  this.start = function (store) {
+	    return function (next) {
+	      return function (action) {
+	        if (!store) return;
+	        _this.stateInfo(store);
+	        _this.actionInfo(next, action);
+	      };
+	    };
+	  };
+
+	  this.stateInfo = function (store) {
+	    console.log('%cAPPLICATION CURRENT STATE', 'color: #528ef1');
+	    console.table(store.getState());
+	  };
+
+	  this.actionInfo = function (next, action) {
+	    var result = next(action);
+	    console.log('%cTRIGGERED ACTION:' + action.type, 'color: #fbbc05');
+	    console.table({ action: result });
+	  };
+	};
+
+	exports.default = reTrace;
+
+/***/ }
+/******/ ])
 });
-
-
-exports['default'] = ReduxTrace;
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function ReduxTrace() {
-    var _this = this;
-
-    _classCallCheck(this, ReduxTrace);
-
-    this.start = function (store) {
-        return function (next) {
-            return function (action) {
-                _this.stateInfo(store);
-                _this.actionInfo(next, action);
-            };
-        };
-    };
-
-    this.stateInfo = function (store) {
-        console.log("%cAPPLICATION CURRENT STATE", 'color: #528ef1');
-        console.table(store.getState());
-    };
-
-    this.actionInfo = function (next, action) {
-        var result = next(action);
-        console.log("%cTRIGGERED ACTION: " + action.type, 'color: #fbbc05');
-        console.table({ "action": result });
-    };
-}
-
-module.exports = exports['default'];
+;
