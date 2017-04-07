39f00ff9eb5e83ecd618e58cc92371b4b3c4c972
Hajji Tarik <hajji-tarik@outlook.fr>
2017-04-07 22:00:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-export default class reTrace {
-  start = store => next => (action) => {
-    if (!store) return;
-    this.stateInfo(store);
-    this.actionInfo(next, action);
-  };
-  stateInfo = (store) => {
-    console.log('%cAPPLICATION CURRENT STATE', 'color: #528ef1');
-    console.table(store.getState());
-  }
-
-  actionInfo = (next, action) => {
-    const result = next(action);
-    console.log(`%cTRIGGERED ACTION:${action.type}`, 'color: #fbbc05');
-    console.table({ action: result });
-  }
+stateInfo = (store) => {
+  console.log('%cAPPLICATION CURRENT STATE', 'color: #528ef1');
+  console.table(store.getState());
 }
+
+actionInfo = (next, action) => {
+  const result = next(action);
+  console.log(`%cTRIGGERED ACTION:${action.type}`, 'color: #fbbc05');
+  console.table({ action: result });
+}
+export const agent = store => next => (action) => {
+    if (!store) return;
+    stateInfo(store);
+    actionInfo(next, action);
+};
